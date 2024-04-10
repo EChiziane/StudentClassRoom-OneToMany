494cfeaf2d38fd0cf0d97faca6b48f71a0ccda78
Ednilson Chiziane <eddybruno43@gmail.com>
2024-04-10 09:38:41 +0200

    Initial commit

---
+++ src/main/java/com/api/studentclassroom/StudentClassRoomApplication.java
@@ -0,0 +1,13 @@
+package com.api.studentclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClassRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClassRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/studentclassroom/StudentClassRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.studentclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClassRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
